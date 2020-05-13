5cb88a958bcbbfd03eaf53b6d6a743c548bdefdc
Darin Kuo <darinkuo@gmail.com>
2020-05-12 17:18:20 -0700

    added simple c file to test m5ops

---
+++ m5ops_example/hello.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include </home/dkuo/gem5/include/gem5/m5ops.h>
+
+int main()
+{
+    m5_dump_reset_stats(1,0);
+    printf("Hello World\n");
+    m5_dump_reset_stats(1,0);
+    return 0;
+    m5_dump_reset_stats(1,0);
+}
