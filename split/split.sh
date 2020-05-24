06f7543b5ec9caa4090de832fe212e5f2ee9e011
Darin Kuo <darinkuo@gmail.com>
2020-05-23 22:13:58 -0700

    reimplemented whitespace removal

--- split/split.sh
+++ split/split.sh
@@ -7,18 +7,24 @@
 # Example
 # ./divide-stats stats.txt output
 
-if [ $# -lt 2 ]
+# If first line is empty, strip from stats
+function strip {
+    local firstLine=$(head -1 $1)
+    if [ -z "$firstLine" ];
     then
+        sed -i '1d' $1
+    fi
+}
+
+if [ $# -lt 2 ]; then
         echo "./split.sh <stats.txt> <output directory name>"
     else
         echo 'Dividing gem5 stats.txt'
         echo '----------------------------------------------'
-        echo 'Creating output directory ' $2
+        echo 'Creating output directory' $2
         mkdir -p $2
         echo '----------------------------------------------'
-        echo 'Splitting stats files: ' $1
+        echo 'Splitting stats files:' $1
+        strip $1
         csplit -z -f $2'/dump' $1 '/---------- Begin Simulation Statistics ----------/' '{*}'
-
-        # remove the first file since it's just whitespace (temporary solution)
-        rm $2/dump00
 fi
